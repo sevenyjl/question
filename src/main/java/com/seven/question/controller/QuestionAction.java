48e59012c5a02c24e3a89d0211a8fff1eef4e252
sevenyjl <50271500+sevenyjl@users.noreply.github.com>
2021-10-12 11:18:15 +0800

    Update QuestionAction.java

--- src/main/java/com/seven/question/controller/QuestionAction.java
+++ src/main/java/com/seven/question/controller/QuestionAction.java
@@ -1,15 +1,21 @@
 package com.seven.question.controller;
 
 import cn.hutool.core.util.StrUtil;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
+
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
 import com.seven.question.service.QuestionService;
 import com.seven.question.entity.Question;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -26,13 +32,16 @@ import java.util.List;
 @RequestMapping("/question")
 public class QuestionAction {
 
-
     @Autowired
     private QuestionService questionService;
 
     @GetMapping("index")
-    public String index(Model model) {
+    public String index(@RequestParam(required = false) boolean hideAnswer, Model model) {
+
         List<Question> list = questionService.list();
+        if (hideAnswer){
+            list.forEach(s-> s.setHideAnswer(true));
+        }
         model.addAttribute("list", list);
         return "index";
     }
