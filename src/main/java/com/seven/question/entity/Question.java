4c91414e14d571dbf52d59415d36b89a236eb9f6
sevenyjl <50271500+sevenyjl@users.noreply.github.com>
2021-10-12 11:19:45 +0800

    Update Question.java

--- src/main/java/com/seven/question/entity/Question.java
+++ src/main/java/com/seven/question/entity/Question.java
@@ -1,17 +1,27 @@
 package com.seven.question.entity;
+
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+
 import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  * <p>
- * 
+ *
  * </p>
  *
  * @author seven
  * @since 2021-10-11
  */
 @TableName("question")
+@Getter
+@Setter
+@ToString
 public class Question implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -52,97 +62,13 @@ public class Question implements Serializable {
     /**
      * 科目
      */
-    private String suject;
+    private String subject;
 
     /**
      * 题型
      */
     private String qType;
 
-    public Integer getId() {
-        return id;
-    }
+    private boolean hideAnswer;
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getOptions() {
-        return options;
-    }
-
-    public void setOptions(String options) {
-        this.options = options;
-    }
-
-    public String getAnswer() {
-        return answer;
-    }
-
-    public void setAnswer(String answer) {
-        this.answer = answer;
-    }
-
-    public String getParsing() {
-        return parsing;
-    }
-
-    public void setParsing(String parsing) {
-        this.parsing = parsing;
-    }
-
-    public String getRemark() {
-        return remark;
-    }
-
-    public void setRemark(String remark) {
-        this.remark = remark;
-    }
-
-    public String getOriginalInformation() {
-        return originalInformation;
-    }
-
-    public void setOriginalInformation(String originalInformation) {
-        this.originalInformation = originalInformation;
-    }
-
-    public String getSuject() {
-        return suject;
-    }
-
-    public void setSuject(String suject) {
-        this.suject = suject;
-    }
-
-    public String getqType() {
-        return qType;
-    }
-
-    public void setqType(String qType) {
-        this.qType = qType;
-    }
-
-    @Override
-    public String toString() {
-        return "Question{" +
-        ", id=" + id +
-        ", title=" + title +
-        ", options=" + options +
-        ", answer=" + answer +
-        ", parsing=" + parsing +
-        ", remark=" + remark +
-        ", originalInformation=" + originalInformation +
-        ", suject=" + suject +
-        ", qType=" + qType +
-        "}";
-    }
 }
