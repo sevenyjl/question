2c3272ad8575c30e6b3a6419f2ce79324aff9275
1399435579@qq.com <1399435579@qq.com>
2021-10-31 18:06:51 +0800

    整合sql

--- src/test/java/com/seven/question/QuestionApplicationTests.java
+++ src/test/java/com/seven/question/QuestionApplicationTests.java
@@ -1,5 +1,7 @@
 package com.seven.question;
 
+import cn.hutool.core.io.FileUtil;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.seven.question.entity.Question;
 import com.seven.question.service.QuestionService;
 
@@ -7,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -21,12 +24,27 @@ class QuestionApplicationTests {
     @Autowired
     QuestionService questionService;
 
+
+    @Test
+    void sort() {
+        int id = 1;
+        List<String> strings = FileUtil.readLines("E:\\aaa-project\\question\\src\\main\\resources\\sql\\question.sql", StandardCharsets.UTF_8);
+        for (String string : strings) {
+            String str = "INSERT INTO `question` VALUES (";
+            int i = string.indexOf(str);
+            if (i != -1) {
+                FileUtil.appendString(str + id + string.substring(string.indexOf(", '")) + "\n", "E:\\aaa-project\\question\\src\\main\\resources\\sql\\question2.sql", StandardCharsets.UTF_8);
+                id++;
+            }
+        }
+    }
+
     @Test
     void listSameQuestion() {
         // 使用前请备份数据
         List<Question> list = questionService.list();
         Map<String, List<Question>> collect = list.stream()
-            .collect(Collectors.groupingBy(s -> s.getTitle() + s.getOptions()));
+                .collect(Collectors.groupingBy(s -> s.getTitle() + s.getOptions()));
         collect.forEach((k, v) -> {
             if (v.size() > 1) {
                 v.remove(0);
