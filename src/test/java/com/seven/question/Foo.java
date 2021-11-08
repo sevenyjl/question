f08d66cfadabfd80457ef5c57fad4423f09d2816
1399435579@qq.com <1399435579@qq.com>
2021-11-08 21:43:24 +0800

    学习与练习

--- src/test/java/com/seven/question/Foo.java
+++ src/test/java/com/seven/question/Foo.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileFilter;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -12,7 +13,16 @@ import cn.hutool.core.io.FileUtil;
 
 public class Foo {
     public static void main(String[] args) {
-
+        Comparator<Integer> df = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1 > o2 ? 1 :
+                        (o1 == o2) ?
+                                0 : -1;
+            }
+        };
+        System.out.println(df.compare(new Integer(1),new Integer(1)));
+        System.out.println(df.compare(1,1));
     }
 
     public static double sqrt(double a) {
